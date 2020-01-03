43c5bcfd9f78f917036144a06c6e5cdaad379cd0
yonghyun Jeong <jayjay@kakao.com>
2020-01-03 16:08:48 +0900

    Add spring project for danawaboshow

---
+++ BoShow/src/main/java/com/yonghyun/BoShow/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.yonghyun.BoShow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping({ "/", "/home" })
+	public String index() {
+		return "index";
+	}
+}
