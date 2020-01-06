5c2d76b0b1fdaea33b186b69e9915cc98aa980d8
yonghyun Jeong <jayjay@kakao.com>
2020-01-06 16:49:47 +0900

    Modify setting.html / Add userInfo.html

--- BoShow/src/main/java/com/yonghyun/BoShow/controller/UserController.java
+++ BoShow/src/main/java/com/yonghyun/BoShow/controller/UserController.java
@@ -15,43 +15,64 @@ import com.yonghyun.BoShow.repository.UserRepository;
 public class UserController {
 	@Autowired
 	UserRepository userRepository;
-	
+
 	@GetMapping({ "/signup" })
 	public String signup() {
-	    return "signup";
+		return "signup";
 	}
-	
+
 	@PostMapping("/signup")
 	public String signupPost(@ModelAttribute User user) {
 		userRepository.save(user);
 		return "redirect:/";
 	}
-	
+
 	@Autowired
 	HttpSession session;
 
 	@GetMapping("/signin")
 	public String signin() {
-	    return "signin";
+		return "signin";
 	}
 
 	@PostMapping("/signin")
 	public String signinPost(@ModelAttribute User user) {
-	    User dbUser = userRepository.findByEmailAndPwd(user.getEmail(), user.getPwd());
-	    if (dbUser != null) {
-	        session.setAttribute("user_info", dbUser);
-	    }
-	    return "redirect:/";
+		User dbUser = userRepository.findByEmailAndPwd(user.getEmail(), user.getPwd());
+		if (dbUser != null) {
+			session.setAttribute("user_info", dbUser);
+		}
+		return "redirect:/";
 	}
-	
+
 	@GetMapping("/signout")
 	public String signout() {
-	    session.invalidate();
-	    return "redirect:/";
+		session.invalidate();
+		return "redirect:/";
 	}
-	
+
 	@GetMapping({ "/setting" })
 	public String setting() {
-	    return "setting";
+		return "setting";
+	}
+
+	@PostMapping("/setting")
+	public String settingPost(@ModelAttribute User user) {
+		User sessionUser = (User) session.getAttribute("user_info");
+		long id = sessionUser.getId();
+		String email = sessionUser.getEmail();
+		String pwd = sessionUser.getPwd();
+		String name = sessionUser.getName();
+		user.setId(id);
+		user.setEmail(email);
+		user.setPwd(pwd);
+		user.setName(name);
+		session.setAttribute("user_info", user);
+		userRepository.save(user);
+		return "setting";
+	}
+
+	@GetMapping({ "/userInfo" })
+	public String userInfo() {
+		return "userInfo";
 	}
 }
