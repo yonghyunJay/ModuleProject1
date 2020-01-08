a655e556eefa7664b3c76fc88cf59d476ba2f429
ddusi <knormla76@naver.com>
2020-01-08 15:56:37 +0900

    20200108 // Padding_Modified

--- BoShow/src/main/java/com/yonghyun/BoShow/controller/HomeController.java
+++ BoShow/src/main/java/com/yonghyun/BoShow/controller/HomeController.java
@@ -3,9 +3,6 @@ package com.yonghyun.BoShow.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.yonghyun.BoShow.model.Data;
-import com.yonghyun.BoShow.model.User;
-
 @Controller
 public class HomeController {
 	@GetMapping({ "/", "/home" })
